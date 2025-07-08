3a7ad75c21f91dfb9667e2561dc8bf38851866fc
NilSlider <nils.schw@hotmail.com>
2025-07-08 18:21:50 +0200

    Cleanup

--- Test.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-
-app = FastAPI()
-
-@app.get("/recently-played")
-def get_recently_played():
-    return {"message": "This route works!"}
